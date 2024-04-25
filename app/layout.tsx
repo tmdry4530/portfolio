9c5f96e99cd21108fe10b6f33e12a1ce6ddc5135
chamdom <wjdtmdry88@gmail.com>
2024-04-25 20:59:08 +0900

    thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,8 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
-// Font 설정
 const inter = Inter({ subsets: ["latin"] });
-
-// Metadata 타입 정의
-interface OGImage {
-  url: string;
-}
-
-interface OpenGraphMetadata {
-  type: string;
-  url: string;
-  description: string;
-  siteName: string;
-  images: OGImage[]; // 이미지 배열의 타입을 명확히 선언
-}
-
-interface Metadata {
-  openGraph: OpenGraphMetadata;
-  title: string;
-  description: string;
-}
 
 export const metadata: Metadata = {
   openGraph: {
@@ -47,20 +27,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:type" content={metadata.openGraph.type} />
-        <meta property="og:url" content={metadata.openGraph.url} />
-        <meta
-          property="og:description"
-          content={metadata.openGraph.description}
-        />
-        <meta property="og:site_name" content={metadata.openGraph.siteName} />
-        {metadata.openGraph.images.map((image, index) => (
-          <meta key={index} property="og:image" content={image.url} />
-        ))}
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
